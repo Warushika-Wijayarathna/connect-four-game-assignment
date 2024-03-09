72485bb866a9c12b930f416868bfb963b3e8f5cf
Warushika-Wijayarathna <warushikaWijayarathna@gmail.com>
2024-03-09 16:12:09 +0530

    HumanPlayer completion

--- src/main/java/lk/ijse/dep/service/AiPlayer.java
+++ src/main/java/lk/ijse/dep/service/AiPlayer.java
@@ -6,5 +6,23 @@ public class AiPlayer extends Player{
     }
     @Override
     public void movePiece(int col){
+        if (col != -1) {
+            return;
+        }
+
+        int randCol;
+        do{
+           randCol=(int) (Math.random()*Board.NUM_OF_COLS);
+        }while (!board.isLegalMove(randCol));
+
+        board.updateMove(randCol, Piece.GREEN);
+        board.getBoardUI().update(randCol,false);
+
+        Winner winner = board.findWinner();
+        if (winner.getWinningPiece() != Piece.EMPTY) {
+            board.getBoardUI().notifyWinner(winner);
+        } else if (!board.existLegalMoves()) {
+            board.getBoardUI().notifyWinner(new Winner(Piece.EMPTY));
+        }
     }
 }
